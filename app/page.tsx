e11582edf11c512e1f8418aff4cc6190ac2726f3
Your Name <mikeatlanta+github@gmail.com>
2026-03-19 12:46:04 -0400

    init

---
+++ app/page.tsx
@@ -0,0 +1,238 @@
+Documentation
+  WFM Request Anything Admin
+    Getting started with Request Anything for administrators
+    Request Anything overview for administrators
+    Managing rules
+      Create a rule
+        General rule parameters
+        Roles and actions parameters
+        Validations
+        Filing rules
+        Auto Processing
+        Notifications
+      View a rule
+        Rule column descriptions
+        Activate a rule
+        Inactivate a rule
+        Edit a rule
+        Delete a rule
+  WFM Request Anything Manager
+    Getting started with Request Anything for managers
+    Request Anything overview for managers
+    Managing employee event requests
+      View employee requests
+        Event request columns
+        View employee request details
+        View validation details
+        Approve a request
+        Deny a request
+  WFM Strategic Planner
+    Strategic Planner overview
+      Welcome to Strategic Planner
+      Introducing Scenarios
+      Scenarios
+      Creating Scenarios
+        Monthly Scenarios
+        Weekly Scenarios
+      Scenario Panes
+        The Work Queues Pane
+        The Staffing Profiles Pane
+        The Totals Pane
+        Summary Columns
+        Running Simulations
+        Optimizing
+        Using Views
+      Comparing Scenarios and Data
+        Comparing Scenarios
+        Comparing Individual Data
+      Exporting to an Excel Report
+        What the Excel Report Contains
+          Title Worksheet
+          Summary Worksheets
+          Work Queue Worksheets
+          Staffing Profile Worksheets
+      Working with Work Queues
+        Editing Work Queues
+        Entering Data Manually in Work Queues
+        Running the Simulator for Work Queues
+        Importing Volume and AHT Information
+          Importing From Forecasting and Scheduling
+          Importing from a File
+            Format of the Import File
+            Importing from All Work Queues
+            Creating a New Work Queue
+            Adding Employees to a Work Queue
+      Working with Staffing Profiles
+        The General Tab
+        The Hire or Transfer Tab
+        The Holidays Tab
+        The Shrinkage Tab
+        Entering Data Manually in Staffing Profiles
+        Modeling Attrition
+        Creating Additional Staffing Profiles
+        Setting Up Transfers for Optimization
+      Exporting and Importing Vacation Hours to Support Vacation Time Optimization
+        Exporting Vacation Hours
+        Importing Vacation Hours
+      Advanced Work with Scenarios
+        Optimizing Staffing Plans
+          Staffing Preferences
+        Advanced Options for Hiring, Transfers, and Overtime
+          Setting Limits on Hiring and Planned Reductions
+          Setting Limits on Transfers
+          Setting Limits on Overtime Hours
+          Performing the Optimization
+          Cost Calculation
+        Import Data
+          Importing Data
+          Importing Data from All Queues
+          Export to Excel
+        Comparison Graph
+          Graphing Data
+          Printing Graphs
+        Running the Simulator
+        Configuring Cost Estimates
+      Time Banking
+        Time Banking in Strategic Planner
+        General Work Flow for Time Banking Plans
+      Strategic Forecasting
+        Strategic Forecasting overview
+        Importing Strategic Forecasting Data
+        Marking Days in Strategic Forecasting
+          Specifying Outliers
+          Specifying Special Events
+          Specifying Reconfiguration Days
+          Additional Controls
+        Viewing the Strategic Forecasting Main Window
+          V and AHT Graphs
+          Date Range
+          Strategic Forecasting additional controls
+          Viewing Week Details
+        Exporting Strategic Forecast Data
+          Exporting Single or Multiple Work Queues to the Database
+          Exporting to a File
+          Setting Up the Strategic Forecast
+  WFM TimeFlex Admin Guide
+    Introduction to TimeFlex
+      TimeFlex overview
+      TimeFlex workspace
+      TimeFlex rules and limitations
+    System settings
+      Define lockout periods
+      Search for and manage users
+      Shut down TimeFlex for all users
+    Set global settings
+      Global configuration settings
+    Campaign settings
+      Define general campaign settings
+    Understanding the TimeFlex pricing model
+      Define the pricing model
+      Automate the pricing model
+      Define a blackout period
+      Define effective scheduling periods
+    Set campaign settings
+      Handling secondary activities
+      Campaign configuration settings
+  WFM TimeFlex Employee Guide
+    Introduction to TimeFlex
+      TimeFlex overview
+      TimeFlex workspace
+      TimeFlex rules and limitations
+    Changing shifts on your schedule
+      Change the shift start and end time
+      Change the shift length
+      Create a shift
+      Split a shift
+      Delete a shift
+    Understanding My Wallet
+      View your coin expirations
+      View your coin balance history
+  WFM TimeFlex Reports Guide
+    TimeFlex campaign reports
+      View campaign reports
+      Campaign Requests Report
+      Campaign Requests Detailed Report
+      Users Coins Report
+      Days Notice Given Report for campaigns
+      Helpful or Unhelpful Requests Report for campaigns
+      Days Notice Before Shift Change Report
+      Unique Requests Per Day Report
+      Requests Report
+  Workforce Management
+    WFM Essentials
+      Work Administration feature availability
+        Work Administration in WFM Essentials
+      My Home feature availability
+        My Home in WFM Essentials
+      Request Management feature availability
+        Request Management in WFM Essentials
+      Forecasting and Scheduling feature availability
+        Forecasting and Scheduling in WFM Essentials
+    WFM Administration
+      Getting started with WFM
+        Introduction to Workforce Management WFM
+        Generating schedules in WFM
+        Setting up scheduling components
+        Work Administration feature availability for WFM Essentials and Enterprise
+      Work queues
+        Work queues overview
+        Workflow Set up normal work queues
+          Create a normal work queue
+            Work queue settings
+            Edit work queue to map data source groups to normal queues
+            Map data source groups to queues
+            Create a process work queue
+        Workflow Set up work queues for multi site operations
+          Create a distributed or virtual work queue
+          Edit work queue to map child queues to parent queues
+          Map child queues to parent queues
+          Edit a work queue
+          Copy a work queue
+        Workflow Create work queue filters
+          Create work queue attributes
+            Work queue attribute definition fields
+            Edit work queue attributes
+            Delete work queue attributes
+          Select work queue to set attributes
+          Set attribute values for a work queue
+      VCT work queues
+        Define a VCT work queue
+          VCT work queue configuration
+          VCT AHT history and current settings
+          Delete VCT work queue
+      VCT arrival patterns
+        Import a VCT arrival pattern
+        Create a VCT arrival pattern
+          VCT arrival patterns settings
+          Edit a VCT arrival pattern
+          Delete a VCT arrival pattern
+      Activities
+        Workflow Set up activities
+          Create an activity type
+            Predefined activity types
+            Activity type details
+            Example Accrual policy for an employee
+            Edit an activity type
+            Delete an activity type
+          Create an activity
+            Predefined activities
+            Activity details
+            Edit an activity
+            Copy an activity
+            Delete an activity
+        Customize a special activity for an organization
+          Special activity override details
+          Example Override attributes for special activities
+        Workflow Map activities for adherence
+          Select to map activities to alternate activities
+          Map alternate activities for adherence
+        Mapping of activities to reason codes and data sources
+        Workflow Upload Time Collector Mappings
+          Create text file of Time Collector Mappings
+          Upload Time Collector Mappings
+          Time Collector Mappings fields
+          View Time Collector Mappings
+        Edit the mapping of a reason code to an activity
+          Time Collector Mapping Details screen
+          Remove the mapping of a reason code to an activity
+        Monitoring adherence from data sources
